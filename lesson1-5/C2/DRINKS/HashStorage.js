e1c64f2271acc546dc6cb6db791316c95a5a29b6
AlexandrPetkov <petkov.alexandr@gmail.com>
2018-05-08 19:21:14 +0300

    completed task C2

---
+++ lesson1-5/C2/DRINKS/HashStorage.js
@@ -0,0 +1,20 @@
+function HashStorage() {
+    this.hash = {};
+
+
+    this.addValue = function (key, value) {
+        this.hash[key] = value;
+    }
+
+    this.getValue = function (key) {
+        return this.hash[key];
+    }
+
+    this.deleteValue = function (key) {
+        delete this.hash[key];
+    }
+
+    this.getKeys = function () {
+        return Object.keys(this.hash);
+    }
+}
